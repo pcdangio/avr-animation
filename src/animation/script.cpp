04f958394634cd72cd910229ad129f0a69fcd8d8
Paul D'Angio <pcdangio@gmail.com>
2023-05-19 18:03:46 -0400

    script: switched to std library use

--- src/animation/script.cpp
+++ src/animation/script.cpp
@@ -4,146 +4,70 @@ using namespace animation;
 
 // CONSTRUCTORS
 script::script(size_t step_count)
-    : step_count(step_count),
-      m_steps_begin(new script::entry[step_count]),
-      m_steps_end(script::m_steps_begin + script::step_count),
+    : m_steps(step_count),
       m_current_step(nullptr)
 {}
-script::~script()
-{
-    // Clean the step entry array.
-    delete [] script::m_steps_begin;
-}
 
 // CONFIGURATION
-bool script::make_step(size_t index, animation::step* step)
+bool script::set_step(size_t index, const std::shared_ptr<animation::step>& step)
 {
-    // Get pointer to desired entry.
-    script::entry* entry = script::m_steps_begin + index;
-    // Check if desired entry is valid.
-    if(entry >= script::m_steps_end)
+    // Validate index.
+    if(index >= script::m_steps.size)
     {
         return false;
     }
 
-    // Create raw step instance and assign it to the entry.
-    entry->assign(step);
+    // Store step at index.
+    script::m_steps[index] = step;
 
     return true;
 }
-bool script::copy_step(size_t index, size_t copy_index)
+bool script::set_step(size_t index, size_t copy_index)
 {
-    // Get reference to destination and check for validity.
-    script::entry* destination_entry = script::m_steps_begin + index;
-    if(destination_entry >= script::m_steps_end)
+    // Validate indices.
+    if(index >= script::m_steps.size || copy_index >= script::m_steps.size)
     {
         return false;
     }
 
-    // Get reference to step to copy from and check for validity.
-    script::entry* source_entry = script::m_steps_begin + copy_index;
-    if(source_entry >= script::m_steps_end)
-    {
-        return false;
-    }
-
-    // Assign source entry to destination entry.
-    destination_entry->assign(source_entry);
-
-    return true;
-}
-bool script::copy_step(size_t index, size_t copy_index, size_t step_count, size_t copy_count)
-{
-    // Validate counts.
-    if(step_count == 0 || copy_count == 0)
-    {
-        // No copy necessary.
-        return true;
-    }
-
-    // Get reference to start destination and check for validity.
-    script::entry* destination_begin = script::m_steps_begin + index;
-    if(destination_begin >= script::m_steps_end)
-    {
-        return false;
-    }
-
-    // Get reference to end destination and check for validity.
-    script::entry* destination_end = destination_begin + step_count * copy_count;
-    if(destination_end > script::m_steps_end)
-    {
-        return false;
-    }
-
-    // Get reference to start source and check for validity.
-    script::entry* source_begin = script::m_steps_begin + copy_index;
-    if(source_begin >= script::m_steps_end)
-    {
-        return false;
-    }
-
-    // Get reference to end source and check for validity.
-    script::entry* source_end = source_begin + step_count;
-    if(source_end > script::m_steps_end)
-    {
-        return false;
-    }
-
-    // Iterate over number over destination range.
-    script::entry* source;
-    script::entry* destination = destination_begin;
-    while(destination < destination_end)
-    {
-        // Iterate over the source range.
-        for(source = source_begin; source != source_end; ++source)
-        {
-            // Copy source to destination and increment destination pointer.
-            (destination++)->assign(source);
-        }
-    }
+    // Copy source index to destination index.
+    script::m_steps[index] = script::m_steps[copy_index];
 
     return true;
 }
 bool script::clear_step(size_t index)
 {
-    // Get pointer to desired entry.
-    script::entry* entry = script::m_steps_begin + index;
-    // Check if desired entry is valid.
-    if(entry >= script::m_steps_end)
+    // Validate index.
+    if(index >= script::m_steps.size)
     {
         return false;
     }
 
-    // Clear entry.
-    entry->clear();
+    // Reset shared_ptr at index.
+    script::m_steps[index].reset();
 
     return true;
 }
 void script::clear()
 {
-    // Iterate over step entries.
-    for(script::entry* entry = script::m_steps_begin; entry != script::m_steps_end; ++entry)
+    // Reset all shared_ptrs in the array.
+    for(auto entry = script::m_steps.begin(); entry != script::m_steps.end(); ++entry)
     {
-        entry->clear();
+        entry->reset();
     }
 }
 
 // CONTROL
 void script::start()
 {
-    // Validate script.
-    if(script::m_steps_begin == script::m_steps_end)
+    // Set current step to beginning of array.
+    script::m_current_step = script::m_steps.begin();
+
+    // Start step if valid.
+    if(script::m_current_step->valid())
     {
-        // Set current step to nullptr.
-        script::m_current_step = nullptr;
-        return;
+        (*script::m_current_step)->start();
     }
-
-    // Set position to start of step array.
-    script::m_current_step = script::m_steps_begin;
-
-    // Start the current step.
-    script::m_current_step->start();
 }
 void script::stop(bool reset)
 {
@@ -153,6 +77,7 @@ void script::stop(bool reset)
     // Reset states if necessary.
     if(reset)
     {
+        // Call the derived reset_state method.
         reset_state();
     }
 }
@@ -164,18 +89,18 @@ void script::reset_state()
 // RUN
 bool script::run_once()
 {
-    // Validate current step (means that script was started).
-    if(!script::m_current_step)
+    // Validate current step.
+    if(!script::m_current_step->valid())
     {
         // Indicate script is complete.
         return true;
     }
 
     // Run the current step and check if it's complete.
-    if(script::m_current_step->run_once())
+    if((*script::m_current_step)->run_once())
     {
         // Increment the current step and check for script completion.
-        if(++script::m_current_step == script::m_steps_end)
+        if(++script::m_current_step == script::m_steps.end())
         {
             // Reset current step to nullptr.
             script::m_current_step = nullptr;
@@ -184,7 +109,7 @@ bool script::run_once()
         }
 
         // Start the new step.
-        script::m_current_step->start();
+        (*script::m_current_step)->start();
     }
 
     return false;
@@ -192,99 +117,22 @@ bool script::run_once()
 void script::run()
 {
     // Iterate through the step array.
-    for(script::m_current_step = script::m_steps_begin; script::m_current_step != script::m_steps_begin; ++script::m_current_step)
+    for(script::m_current_step = script::m_steps.begin(); script::m_current_step != script::m_steps.end(); ++script::m_current_step)
     {
+        // Validate step.
+        if(!script::m_current_step->valid())
+        {
+            break;
+        }
+        
         // Start the step.
-        script::m_current_step->start();
+        (*script::m_current_step)->start();
 
         // Run the step until its complete.
-        while(!script::m_current_step->run_once())
+        while(!(*script::m_current_step)->run_once())
         {}
     }
 
     // Reset current step to nullptr.
     script::m_current_step = nullptr;
-}
-
-// ENTRY
-
-// CONSTRUCTORS
-script::entry::entry()
-    : m_step(nullptr),
-      m_reference_count(nullptr)
-{}
-script::entry::~entry()
-{
-    // Decrement reference count.
-    script::entry::decrement_reference();
-}
-
-// ASSIGNMENT
-void script::entry::assign(animation::step* step)
-{
-    // Decrement reference count for existing step instance.
-    script::entry::decrement_reference();
-
-    // Create new step and reference count instances.
-    script::entry::m_step = step;
-    script::entry::m_reference_count = new size_t(1);
-}
-void script::entry::assign(const entry* other)
-{
-    // Decrement reference count for existing step instance.
-    script::entry::decrement_reference();
-
-    // Copy step and reference count from other entry.
-    script::entry::m_step = other->m_step;
-    script::entry::m_reference_count = other->m_reference_count;
-
-    // Increment reference count for new step instance.
-    if(script::entry::m_reference_count)
-    {
-        ++(*script::entry::m_reference_count);
-    }
-}
-void script::entry::clear()
-{
-    // Decrement reference count for existing step instance.
-    script::entry::decrement_reference();
-
-    // Set step and reference count to nullptr.
-    script::entry::m_step = nullptr;
-    script::entry::m_reference_count = nullptr;
-}
-
-// RUN
-void script::entry::start()
-{
-    // Check if step exists.
-    if(script::entry::m_step)
-    {
-        // Start step.
-        script::entry::m_step->start();
-    }
-}
-bool script::entry::run_once()
-{
-    // Check if step exists.
-    if(!script::entry::m_step)
-    {
-        // Indicate step is complete since it does not exist.
-        return true;
-    }
-
-    // Run step.
-    return script::entry::m_step->run_once();
-}
-
-// REFERENCE COUNT
-void script::entry::decrement_reference()
-{
-    // Decrement reference count and check if no references left.
-    if(script::entry::m_reference_count && --(*script::entry::m_reference_count) == 0)
-    {
-        // Clean up step and reference count instances.
-        delete script::entry::m_step;
-        delete script::entry::m_reference_count;
-    }
 }
--- src/animation/script.hpp
+++ src/animation/script.hpp
@@ -6,6 +6,9 @@
 // animation
 #include <animation/step.hpp>
 
+// std
+#include <std.hpp>
+
 /// \brief Contains all code for the Arduino animation library.
 namespace animation {
 
@@ -18,27 +21,18 @@ public:
     /// \brief Creates a new script instance.
     /// \param step_count The number of steps in the script.
     script(size_t step_count);
-    ~script();
 
     // CONFIGURATION
-    /// \brief Makes a new step at a specified position in the script.
+    /// \brief Sets the step at a specified position in the script.
     /// \param index The index to assign the new step to.
-    /// \param step The new step instance.
+    /// \param step The step instance to set.
     /// \return TRUE if the step was set successfully, FALSE if index out of range.
-    /// \note Do not reuse step instance pointers. To reuse an existing step, use the copy version of set_step().
-    bool make_step(size_t index, animation::step* step);
+    bool set_step(size_t index, const std::shared_ptr<animation::step>& step);
     /// \brief Copies an existing step to another position in the script.
     /// \param index The index to assign the copied step to.
     /// \param copy_index The index of the step to copy from.
     /// \return TRUE if the step was set successfully, FALSE if either index is out of range.
-    bool copy_step(size_t index, size_t copy_index);
-    /// \brief Copies multiple existing steps to another position in the script.
-    /// \param index The index to start assigning copied steps to.
-    /// \param copy_index The index of the first step in the range to copy.
-    /// \param step_count The number of steps to copy.
-    /// \param copy_count The number of times to copy the range of steps.
-    /// \return TRUE if the steps were copied successfully, FALSE if any indices are out of range.
-    bool copy_step(size_t index, size_t copy_index, size_t step_count, size_t copy_count = 1);
+    bool set_step(size_t index, size_t copy_index);
     /// \brief Clears a step at a desired position in the script.
     /// \param index The index of the step to clear.
     /// \return TRUE if the step was cleared successfully, FALSE if the index is out of range.
@@ -59,8 +53,9 @@ public:
     void run();
 
     // PROPERTIES
-    /// \brief The number of steps in the script.
-    const size_t step_count;
+    /// \brief Get the number of steps in the script.
+    /// \return The number of steps.
+    size_t step_count() const;
 
 protected:
     // CONTROL
@@ -68,51 +63,11 @@ protected:
     virtual void reset_state();
 
 private:
-    // ENTRY
-    /// \brief An animation step entry in the script.
-    /// \details This class allows shared use and management of an animation step pointer.
-    class entry
-    {
-    public:
-        // CONSTRUCTORS
-        /// \brief Creates a new empty entry.
-        entry();
-        ~entry();
-        
-        // ASSIGNMENT
-        /// \brief Assigns a new step instance to this entry.
-        /// \param step The new step instance to assign.
-        void assign(animation::step* step);
-        /// \brief Copies an existing step instance to this entry.
-        /// \param other The existing step instance to copy.
-        void assign(const entry* other);
-        /// \brief Clears this entry.
-        void clear();
-
-        // RUN
-        /// \brief Starts the step instance if it exists.
-        void start();
-        /// \brief Runs the step instance once if it exists.
-        /// \return TRUE if the step instance is complete or does not exist, FALSE if step instance is not complete.
-        bool run_once();
-
-    private:
-        // INSTANCE
-        /// \brief The shared step instance.
-        animation::step* m_step;
-        /// \brief The number of entries currently referencing the shared step instance.
-        size_t* m_reference_count;
-        /// \brief Decrements the reference count of the shared step instance, and frees the instance if there are no more references.
-        void decrement_reference();
-    };
-
     // STEPS
-    /// \brief A pointer to the beginning of the step entry array.
-    script::entry* m_steps_begin;
-    /// \brief A pointer to the end of the step entry array.
-    script::entry* m_steps_end;
-    /// \brief A pointer to the current position in the step entry array.
-    script::entry* m_current_step;
+    /// \brief The array of steps that make up the script.
+    std::array<std::shared_ptr<animation::step>> m_steps;
+    /// \brief The current step being executed in the script.
+    std::iterator<std::shared_ptr<animation::step>> m_current_step;
 };
 
 }
